4cfedcbd9a9639328f195ffe343cf5af83a945e7
Marshall Hahn <git@marshallhahn.com>
2024-05-05 09:37:59 -0700

    message acknowledgment

--- work_queues/worker.rb
+++ work_queues/worker.rb
@@ -9,11 +9,12 @@ queue = channel.queue('hello')
 
 begin
     puts ' [*] Waiting for messages. To exit press CTRL+C'
-    queue.subscribe(block: true) do |delivery_info, _properties, body|
-      puts " [x] Received #{body}"
+    queue.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
+      puts " [x] Received '#{body}'"
       # imitate some work
       sleep body.count('.').to_i
       puts ' [x] Done'
+      channel.ack(delivery_info.delivery_tag)
     end
   rescue Interrupt => _
     connection.close
